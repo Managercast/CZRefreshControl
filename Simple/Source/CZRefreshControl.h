30b369e5e03190ce820c67c60d649462663c3be7
NeXTSTEP <leiggee@126.com>
2016-01-04 12:39:47 +0800

    自定义刷新

---
+++ Simple/Source/CZRefreshControl.h
@@ -0,0 +1,13 @@
+//
+//  CZRefreshControl.h
+//  Simple
+//
+//  Created by NeXTSTEP on 16/1/4.
+//  Copyright © 2016年 NeXTSTEP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZRefreshControl : UIControl
+- (void) startAnimation;
+@end
---
+++ Simple/Source/CZRefreshControl.m
@@ -0,0 +1,17 @@
+//
+//  CZRefreshControl.m
+//  Simple
+//
+//  Created by NeXTSTEP on 16/1/4.
+//  Copyright © 2016年 NeXTSTEP. All rights reserved.
+//
+
+#import "CZRefreshControl.h"
+
+@implementation CZRefreshControl
+
+- (void)startAnimation {
+    NSLog(@"开始刷新");
+}
+
+@end
